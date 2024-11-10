c411c3cd9df7e6cf657ab5be70e501890da496df
Aung Khant <aungkhant@Aungs-MacBook-Air-2.local>
2024-11-10 16:06:56 +0700

    adding for day5

--- src/Day5/Function_lec_01.java
+++ src/Day5/Function_lec_01.java
@@ -1,4 +1,50 @@
 package Day5;
 
+import java.util.Scanner;
+
 public class Function_lec_01 {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Information information = getInformation();
+        boolean response= processOfBooking(information);
+        if (response){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+    public static Information getInformation() {
+        Information information = new Information();
+        System.out.println("Enter pick-up-location:");
+        information.pickUpLocation = scanner.nextLine();
+        System.out.println("Enter destination:");
+        information.destination = scanner.nextLine();
+        System.out.println("Enter time:");
+        information.time = scanner.nextLine();
+        System.out.println("Enter number-of-seats:");
+        information.numberOfSeats = scanner.nextInt();scanner.nextLine();
+        System.out.println("Enter number of phone-number:");
+        information.phNumber = scanner.nextLine();
+        return information;
+    }
+
+    public static class Information{
+        String pickUpLocation;
+        String destination;
+        String time;
+        int numberOfSeats;
+        String phNumber;
+    }
+    public static boolean processOfBooking(Information information) {
+        if (information.numberOfSeats >20 || information.pickUpLocation.equals("")
+                || information.destination.equals("")||!(information.destination.equals("mega bangna")||information.destination.equals("siam"))
+                || information.time.equals("") || information.phNumber.equals("")) {
+            return false;
+
+        }
+        System.out.println("Process of Booking is Completed");
+        System.out.println("Process of Booking is Completed");
+        return true;
+    }
 }
